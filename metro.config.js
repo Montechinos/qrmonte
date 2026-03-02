8692e8fc19977224f7519e0c23275a0995ae5d3b
mateo.montesinos.j <mateo.montesinos.j@gmail.com>
2026-03-02 21:01:33 +0000

    fix: instalar firebase y corregir resolución de módulos en Metro - npm install firebase - Agregar sourceExts con "cjs" en metro.config.js - Desactivar unstable_enablePackageExports para compatibilidad con Firebase modular

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+// Necesario para Firebase modular
+config.resolver.sourceExts = [...config.resolver.sourceExts, "cjs"];
+config.resolver.unstable_enablePackageExports = false; // ← clave para Firebase
+
+module.exports = withNativeWind(config, { input: "./global.css" });
